55ef7c1919fa1a805ced6adaf4b1d1644a273233
Jak Wings <jakwings@gmail.com>
2015-05-12 04:18:01 +0800

    Follow the official implementation
    1. Return the directory path as a string instead of an array of strings.
    2. Guess the path from the widely used environment variables.
    3. Dynamically generate the path and do not cache it.
    
    These are all breaking changes.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = require('osenv').tmpdir()
+var isWindows = process.platform === 'win32'
 
 
+module.exports = function () {
+  if (isWindows) {
+    return process.env.TEMP ||
+      process.env.TMP ||
+      (process.env.SystemRoot || process.env.windir) + '\\temp'
+  } else {
+    return process.env.TMPDIR ||
+      process.env.TMP ||
+      process.env.TEMP ||
+      '/tmp'
+  }
+}
